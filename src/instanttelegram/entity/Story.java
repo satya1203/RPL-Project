44a8bfd5810269b56a04540077edffc2ec6a7361
Melvin ST <67085652+melvin16s@users.noreply.github.com>
2021-03-27 20:10:07 +0700

    Update Story

--- src/instanttelegram/entity/Story.java
+++ src/instanttelegram/entity/Story.java
@@ -3,12 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package instanttelegram.entity;
+
+import java.util.Date;
 
 /**
  *
  * @author MelvinS
  */
 public class Story {
+    private Date storyTime;
+    private User author;
+    private String storySrc;
+
+    public Story(Date storyTime, User author, String storySrc) {
+        this.storyTime = storyTime;
+        this.author = author;
+        this.storySrc = storySrc;
+    }
+
+    public Date getStoryTime() {
+        return storyTime;
+    }
+
+    public void setStoryTime(Date storyTime) {
+        this.storyTime = storyTime;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getStorySrc() {
+        return storySrc;
+    }
+
+    public void setStorySrc(String storySrc) {
+        this.storySrc = storySrc;
+    }
+    
     
 }
