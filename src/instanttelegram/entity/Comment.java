aa078846aa6caab20a1af3f98bcde5812ef910e2
Melvin ST <67085652+melvin16s@users.noreply.github.com>
2021-03-27 19:57:36 +0700

    Update Comment

--- src/instanttelegram/entity/Comment.java
+++ src/instanttelegram/entity/Comment.java
@@ -3,12 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package instanttelegram.entity;
+
+import java.util.Date;
 
 /**
  *
  * @author MelvinS
  */
 public class Comment {
+    private User author;
+    private Date commentTime;
+    private String comment;
+
+    public Comment(User author, Date commentTime, String comment) {
+        this.author = author;
+        this.commentTime = commentTime;
+        this.comment = comment;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Date getCommentTime() {
+        return commentTime;
+    }
+
+    public void setCommentTime(Date commentTime) {
+        this.commentTime = commentTime;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    
     
 }
